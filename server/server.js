21a6fc04cece29974d766624f87d877cd2ba6478
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2022-09-22 11:13:07 +0600

    setup mongo db atlas

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT  = 8081;
@@ -6,6 +7,9 @@ const PORT  = 8081;
 app.get('/', (req, res) => {
     res.send("Hello World")
 });
+
+await mongoose.connect('mongodb+srv://mdrabiulhasan:mdrabiulhasan@mern-expense.ecd8t5c.mongodb.net/?retryWrites=true&w=majority');
+console.log("mongo atlas database are connected");
 
 app.listen( PORT, () => {
     console.log("Your application are running http://localhost:" + PORT);
